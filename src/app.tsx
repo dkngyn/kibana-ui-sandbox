2025bf8be7786a1ed65312508db5d545dca95ce0
Dan Nguyen <dk4nguyen@gmail.com>
2021-03-04 14:33:03 -0800

    Add sass stylesheet
    - mirror node-sass dependency
    - add eui theme and global styling
    - setup main app page layout
    - clean up public dir

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export function App() {
-  return <main className="container">app</main>;
+  return (
+    <div className="euiPage">
+      <div className="euiPageBody euiPageBody--paddingLarge">
+        <div className="euiPageContent euiPageContent--restrictWidth">App</div>
+      </div>
+    </div>
+  );
 }
