6d82d3f3ce44df3d415f71ea8efe0880aee8fbd2
Dan <dan.nguyen@imperva.com>
2021-03-08 09:22:42 -0800

    add week days

--- src/components/day_time_picker/day_time_picker.tsx
+++ src/components/day_time_picker/day_time_picker.tsx
@@ -2,13 +2,29 @@ import React, { PureComponent } from 'react';
 import moment from 'moment';
 // @ts-ignore
 import { EuiDatePicker, EuiFormRow } from '@elastic/eui';
+import { dayOfWeekCodes } from './consts';
 
 export class DayTimePicker extends PureComponent {
   public render() {
     return (
-      <EuiFormRow label="Select a date">
-        <EuiDatePicker selected={moment()} onChange={() => {}} />
-      </EuiFormRow>
+      <>
+        <EuiFormRow label="Select a date">
+          <EuiDatePicker selected={moment()} onChange={() => {}} />
+        </EuiFormRow>
+        <br />
+        <hr />
+        <br />
+        <div className="daytime-picker">{this.renderDaysOfWeek()}</div>
+      </>
     );
+  }
+
+  private renderDaysOfWeek() {
+    const weekDays = Object.values(dayOfWeekCodes).map((weekday, i) => (
+      <div key={i} className="daytime-picker__weekday">
+        {weekday}
+      </div>
+    ));
+    return <div className="daytime-picker__weekdays">{weekDays}</div>;
   }
 }
